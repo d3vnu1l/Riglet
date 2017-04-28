cbce3416d46903ba37626e4eb5026dfb5dcccf46
Unknown <radeushane@gmail.com>
2017-04-28 11:28:27 -0700

    test

--- riglettemp/app/src/main/java/com/example/ryan/riglettemp/HomeActivity.java
+++ riglettemp/app/src/main/java/com/example/ryan/riglettemp/HomeActivity.java
@@ -2,8 +2,17 @@ package com.example.ryan.riglettemp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+    //Faux Tab Bar
+    private Button home;
+    private Button friendsList;
+    private Button addFriend;
+    private Button settings;
+    private Button logOut;
+    //
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- riglettemp/app/src/main/java/com/example/ryan/riglettemp/LoginActivity.java
+++ riglettemp/app/src/main/java/com/example/ryan/riglettemp/LoginActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
