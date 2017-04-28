8c27efb000ac1665ce831f673b67cbed2b5d1c6a
Unknown <radeushane@gmail.com>
2017-04-28 12:44:40 -0700

    test
    ignore

--- riglettemp/app/src/main/java/com/example/ryan/riglettemp/EditFriendsActivity.java
+++ riglettemp/app/src/main/java/com/example/ryan/riglettemp/EditFriendsActivity.java
@@ -9,5 +9,6 @@ public class EditFriendsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_friends);
+        int test;
     }
 }
