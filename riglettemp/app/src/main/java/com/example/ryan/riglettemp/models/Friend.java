63817f0507ab8625b42a93bae77d6eca7d70b6bd
Unknown <radeushane@gmail.com>
2017-05-01 00:29:29 -0700

    Updated models

--- riglettemp/app/src/main/java/com/example/ryan/riglettemp/models/Friend.java
+++ riglettemp/app/src/main/java/com/example/ryan/riglettemp/models/Friend.java
@@ -11,4 +11,16 @@ public class Friend{
     private String lastName;
     private boolean gender;
     private List<Message> Messages;
+
+    public Friend(String firstName, String lastName, boolean gender, List<Message> Messages){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.Messages = Messages;
+    }
+
+    public String getFirstName(){ return firstName; }
+    public String getLastName(){ return lastName;}
+    public boolean getGender(){ return gender;}
+    public List<Message> getMessages(){ return Messages;}
 }
--- riglettemp/app/src/main/java/com/example/ryan/riglettemp/models/User.java
+++ riglettemp/app/src/main/java/com/example/ryan/riglettemp/models/User.java
@@ -12,4 +12,20 @@ public class User{
     private boolean gender;
     private List<Friend> Friends;
 
+    public User(String firstName, String lastName, boolean gender, List<Friend> Friends){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.Friends = Friends;
+    }
+
+    public String getFirstName(){ return firstName; }
+    public String getLastName(){ return lastName;}
+    public boolean getGender(){ return gender;}
+    public List<Friend> getFriends(){ return Friends;}
+
+    public String getFirstName(Friend friend){ return friend.getFirstName(); }
+    public String getLastName(Friend friend){ return friend.getLastName();}
+    public boolean getGender(Friend friend){ return friend.getGender();}
+    public List<Message> getMessages(Friend friend){ return friend.getMessages();}
 }
