97c577551c038d2e6bb06a51c6660f2698705bee
Unknown <radeushane@gmail.com>
2017-05-05 11:33:08 -0700

    Removed DOB field from signup

--- riglettemp/app/src/main/java/com/example/ryan/riglettemp/SignupActivity.java
+++ riglettemp/app/src/main/java/com/example/ryan/riglettemp/SignupActivity.java
@@ -13,7 +13,6 @@ import com.example.ryan.riglettemp.models.User;
 public class SignupActivity extends AppCompatActivity {
     private EditText enter_fn;
     private EditText enter_ln;
-    private EditText enter_DOB;
     private EditText enter_pass;
     private EditText reEnter_pass;
     private EditText enter_gender;
@@ -29,7 +28,6 @@ public class SignupActivity extends AppCompatActivity {
 
         enter_fn = (EditText) findViewById(R.id.enter_fn);
         enter_ln = (EditText) findViewById(R.id.enter_ln);
-        enter_DOB = (EditText) findViewById(R.id.enter_DOB);
         enter_pass = (EditText) findViewById(R.id.enter_pass);
         reEnter_pass = (EditText) findViewById(R.id.reEnter_pass);
         enter_gender = (EditText) findViewById(R.id.enter_gender);
@@ -41,14 +39,13 @@ public class SignupActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String fn = enter_fn.getText().toString().trim();
                 String ln = enter_ln.getText().toString().trim();
-                String dOB = enter_DOB.getText().toString().trim();
                 String pass = enter_pass.getText().toString().trim();
                 String repass = reEnter_pass.getText().toString().trim();
                 String uID = enter_id.getText().toString().trim();
                 String gen = enter_gender.getText().toString().trim();
 
                 //check for all fields entry
-                if (fn.equals("") || ln.equals("") || dOB.equals("") || pass.equals("") || repass.equals("") || gen.equals("") || uID.equals("")) {
+                if (fn.equals("") || ln.equals("") || pass.equals("") || repass.equals("") || gen.equals("") || uID.equals("")) {
                     Toast.makeText(SignupActivity.this, "Please complete all fields.", Toast.LENGTH_SHORT).show();
                 }
                 //need to check that uid is unique here
