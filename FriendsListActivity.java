2ab4b2a6909cc9de326e66264ad650cc8c5b498e
lmperez2 <lmperez@cpp.edu>
2017-05-07 22:40:00 -0700

    Add files via upload

---
+++ FriendsListActivity.java
@@ -0,0 +1,101 @@
+package com.example.ryan.riglettemp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FriendsListActivity extends AppCompatActivity {
+    private Button home;
+    private Button friendsList;
+    private Button addFriend;
+    private Button settings;
+    private Button logOut;
+    private List Friends;
+    private Friend friend;
+    private User Me;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends_list);
+
+        Intent i = getIntent();
+        Bundle oldBundle = i.getBundleExtra("bundle");
+        Me = oldBundle.getParcelable("User");
+        i.setExtrasClassLoader(getClassLoader());
+
+        ArrayList<Friend> myFriendList = me.getFriends();
+        ListView listview = (ListView) findViewById(R.id.FriendsListView);
+
+        for (int i = 0; i < this.Friends.size(); i++) {
+            listview
+        }
+
+
+        home = (Button) findViewById(R.id.home);
+        friendsList = (Button) findViewById(R.id.friendsList);
+        addFriend = (Button) findViewById(R.id.addFriend);
+        settings = (Button) findViewById(R.id.settings);
+        logOut = (Button) findViewById(R.id.logOut);
+
+        //Tab Bar
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                i.setExtrasClassLoader(getClassLoader());
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("User", Me);
+                i.putExtra("bundle",bundle);
+                startActivity(i);
+            }
+        });
+        friendsList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), FriendsListActivity.class);
+                i.setExtrasClassLoader(getClassLoader());
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("User", Me);
+                i.putExtra("bundle",bundle);
+                startActivity(i);
+            }
+        });
+        addFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), AddFriendsActivity.class);
+                i.setExtrasClassLoader(getClassLoader());
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("User", Me);
+                i.putExtra("bundle",bundle);
+                startActivity(i);
+            }
+        });
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
+                i.setExtrasClassLoader(getClassLoader());
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("User", Me);
+                i.putExtra("bundle",bundle);
+                startActivity(i);
+            }
+        });
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                i.setExtrasClassLoader(getClassLoader());
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("User", Me);
+                i.putExtra("bundle",bundle);
+                startActivity(i);
+            }
+        });
+    }
+}
